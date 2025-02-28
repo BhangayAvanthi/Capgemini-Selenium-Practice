13e7d587198747a1d5ad84e6347d2c06276a6f59
BhangayAvanthi <137265521+BhangayAvanthi@users.noreply.github.com>
2025-02-28 12:04:16 +0530

    Add files via upload

---
+++ SeleniumHelper.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.BiDi.Modules.BrowsingContext;
+
+namespace TestProject1
+{
+   public class SeleniumHelper
+    {
+        private readonly IWebDriver driver;
+        private readonly WebDriverWait _wait;
+
+        public SeleniumHelper(IWebDriver driver,int timeoutInminss=2)
+        {
+            this.driver = driver;
+            _wait = new WebDriverWait(driver, TimeSpan.FromMinutes(timeoutInminss));
+        }
+
+        public void NavigateUrl(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void ClickElement(By locator)
+        {
+            //the below expectedconditons and elementtobeclickable is handled thoruh the using SeleniumExtras.WaitHelpers; package
+            _wait.Until(ExpectedConditions.ElementToBeClickable(locator)).Click();
+        }
+
+        public void EnterTxt(By locator,string txt)
+        {
+            var element = _wait.Until(ExpectedConditions.ElementExists(locator));
+            element.Clear();
+            element.SendKeys(txt);
+        }
+    }
+}
---
+++ UnitTest2.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject1;
+
+
+namespace TestProject1.Tests
+{
+    class UnitTest2
+    {
+
+        private IWebDriver driver;
+        private SeleniumHelper helper;
+
+        [SetUp]
+        public void setup()
+        {
+            driver = new ChromeDriver();
+            helper = new SeleniumHelper(driver);
+        }
+
+        [Test]
+        public void LoginToWeb()
+        {
+            helper.NavigateUrl("http://eaapp.somee.com/");
+            helper.ClickElement(By.Id("loginLink"));
+            helper.EnterTxt(By.Id("UserName"),"admin");
+            helper.EnterTxt(By.Id("Password"), "password");
+            helper.ClickElement(By.Id("loginIn"));
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
